8bd8a81622efb32fa43b997b32a0023564fd0e13
Abtechguru <careernig24@gmail.com>
2025-07-07 17:12:28 +0100

    frontend

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className="bg-gray-100 min-h-screen flex items-center justify-center ">
+        <h1 className="text-3xl font-bold underline">
+          Welcome to my frontend project
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
