ffc3218b18173e14a2c0e71ceb6a61914ebf4af9
Abtechguru <careernig24@gmail.com>
2025-07-14 01:12:00 +0100

    final touch

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
-import ShopContextProvider from './context.tsx';
-import { AuthProvider } from './auth/AuthContext';
-createRoot(document.getElementById('root')).render(_jsx(BrowserRouter, { children: _jsx(ShopContextProvider, { children: _jsx(AuthProvider, { children: _jsx(App, {}) }) }) }));
